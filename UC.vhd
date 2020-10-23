475bdaec9c4d38d45e0c32fbfb58a9742c61645b
Thomas Queiroz <thomasqueirozb@gmail.com>
2020-10-23 16:21:40 -0300

    Changed signal to constant

--- UC.vhd
+++ UC.vhd
@@ -15,11 +15,11 @@ entity UC is
 end entity;
 
 architecture rtl of UC is
-    signal f_add: std_logic_vector(5 downto 0) := "100000";
-    signal f_sub: std_logic_vector(5 downto 0) := "100010";
-    signal f_and: std_logic_vector(5 downto 0) := "100100";
-    signal f_or: std_logic_vector(5 downto 0) := "100101";
-    signal f_less: std_logic_vector(5 downto 0) := "101010";
+    constant f_add: std_logic_vector(5 downto 0) := "100000";
+    constant f_sub: std_logic_vector(5 downto 0) := "100010";
+    constant f_and: std_logic_vector(5 downto 0) := "100100";
+    constant f_or: std_logic_vector(5 downto 0) := "100101";
+    constant f_less: std_logic_vector(5 downto 0) := "101010";
 begin
     enableWriteD <= '1' when opcode = "000000" else '0';
     commandULA <= "000" when (unsigned(opcode) = 0 and funct = f_add) else -- add
