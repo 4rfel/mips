5c65376ad167970ba2fa651da85146e43c48e401
Arfel <rafael_1999_@hotmail.com.br>
2020-11-08 19:40:08 -0300

    Update bancoRegistradores.vhd

--- bancoRegistradores.vhd
+++ bancoRegistradores.vhd
@@ -32,8 +32,47 @@ architecture comportamento of bancoRegistradores is
     subtype palavra_t is std_logic_vector((larguraDados-1) downto 0);
     type memoria_t is array(2**larguraEndBancoRegs-1 downto 0) of palavra_t;
 
+    function initMemory
+        return memoria_t is variable tmp : memoria_t := (others => (others => '0'));
+    begin
+        -- Inicializa os endereços:
+        tmp(0)  := x"00000000";
+        tmp(1)  := x"00000000";
+        tmp(2)  := x"00000000";
+        tmp(3)  := x"00000000";
+        tmp(4)  := x"00000000";
+        tmp(5)  := x"00000000";
+        tmp(6)  := x"00000000";
+        tmp(7)  := x"00000000";
+        tmp(8)  := x"00000000";
+        tmp(9)  := x"0000000A";
+        tmp(10) := x"0000000B";
+        tmp(11) := x"0000000C";
+        tmp(12) := x"0000000D";
+        tmp(13) := x"00000016";
+        tmp(14) := x"00000000";
+        tmp(15) := x"00000000";
+        tmp(16) := x"00000000";
+        tmp(17) := x"00000000";
+        tmp(18) := x"00000000";
+        tmp(19) := x"00000000";
+        tmp(20) := x"00000000";
+        tmp(21) := x"00000000";
+        tmp(22) := x"00000000";
+        tmp(23) := x"00000000";
+        tmp(24) := x"00000000";
+        tmp(25) := x"00000000";
+        tmp(26) := x"00000000";
+        tmp(27) := x"00000000";
+        tmp(28) := x"00000000";
+        tmp(29) := x"00000000";
+        tmp(30) := x"00000000";
+        tmp(31) := x"00000000";
+        return tmp;
+        end initMemory;
+
     -- Declaracao dos registradores:
-    shared variable registrador : memoria_t;
+    shared variable registrador : memoria_t := initMemory;
 
 begin
     process(clk) is
